7e9063e94866d6f948d5efac6430010140ea01c4
krystiano13 <crystianotv@gmail.com>
2025-01-17 16:39:26 +0100

    install ux-react bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
