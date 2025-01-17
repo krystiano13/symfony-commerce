9d4e19168c4520e8bb44257e63a83cb312d9da25
krystiano13 <crystianotv@gmail.com>
2025-01-17 16:45:49 +0100

    install & configuire webpack for react

--- config/bundles.php
+++ config/bundles.php
@@ -6,5 +6,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
