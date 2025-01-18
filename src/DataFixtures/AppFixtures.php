514bb41bf01175d9a4031f350dc4037bc23a2c40
krystiano13 <crystianotv@gmail.com>
2025-01-18 14:03:27 +0100

    fix files using cs-fixer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,41 +2,41 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Product;
 
 class AppFixtures extends Fixture
 {
     private array $products = [
         [
-            "name" => "Intel Core i5-12500",
-            "amount" => 5,
-            "price" => 779.99,
-            "imageSrc" => "https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2021/12/pr_2021_12_23_9_30_34_58_00.jpg"
+            'name' => 'Intel Core i5-12500',
+            'amount' => 5,
+            'price' => 779.99,
+            'imageSrc' => 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2021/12/pr_2021_12_23_9_30_34_58_00.jpg',
         ],
         [
-            "name" => "AMD Ryzen 5 5600",
-            "amount" => 5,
-            "price" => 439.99,
-            "imageSrc" => "https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2022/4/pr_2022_4_4_13_1_27_284_00.jpg"
+            'name' => 'AMD Ryzen 5 5600',
+            'amount' => 5,
+            'price' => 439.99,
+            'imageSrc' => 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2022/4/pr_2022_4_4_13_1_27_284_00.jpg',
         ],
         [
-            "name" => "Intel Core i5-14400f",
-            "amount" => 5,
-            "price" => 679.99,
-            "imageSrc" => "https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2024/1/pr_2024_1_5_10_17_14_409_00.jpg"
+            'name' => 'Intel Core i5-14400f',
+            'amount' => 5,
+            'price' => 679.99,
+            'imageSrc' => 'https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2024/1/pr_2024_1_5_10_17_14_409_00.jpg',
         ],
     ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach($this->products as $pr) {
+        foreach ($this->products as $pr) {
             $product = new Product();
-            $product -> setName($pr["name"]);
-            $product -> setPrice($pr["price"]);
-            $product -> setAmount($pr["amount"]);
-            $product -> setImageSrc($pr["imageSrc"]);
+            $product->setName($pr['name']);
+            $product->setPrice($pr['price']);
+            $product->setAmount($pr['amount']);
+            $product->setImageSrc($pr['imageSrc']);
 
             $manager->persist($product);
 
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,13 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -23,7 +21,7 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['class' => 'p-inputtext'],
             ])
             ->add('plainPassword', RepeatedType::class, [
-                                // instead of being set onto the object directly,
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
