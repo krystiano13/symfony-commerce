30cb97c505035c210f0560cd3303e5c6cc0ff1b5
krystiano13 <crystianotv@gmail.com>
2025-01-19 16:56:18 +0100

    implement form for creating order

--- assets/react/controllers/Cart.jsx
+++ assets/react/controllers/Cart.jsx
@@ -3,6 +3,8 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { InputMask } from 'primereact/inputmask';
 import styled from "styled-components";
 
 const Main = styled.main`
@@ -64,9 +66,61 @@ export default function Cart(props) {
                 <Card
                     title="Przejdź do transakcji"
                 >
-                    <Button>
-                        Złóż zamówienie
-                    </Button>
+                    <form
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "1rem"
+                        }}
+                    >
+                        <section
+                            style={{
+                                display: "flex",
+                                gap: ".5rem"
+                            }}
+                        >
+                            <InputText
+                                name="name"
+                                placeholder="Imię"
+                                required={true}
+                                style={{ width: "50%" }}
+                            />
+                            <InputText
+                                name="surname"
+                                placeholder="Nazwisko"
+                                required={true}
+                                style={{ width: "50%" }}
+                            />
+                        </section>
+                        <section
+                            style={{
+                                display: "flex",
+                                gap: ".5rem"
+                            }}
+                        >
+                            <InputMask
+                                mask="99-999"
+                                placeholder="Kod Pocztowy"
+                                name="postal_code"
+                                required={true}
+                                style={{ width: "50%" }}
+                            />
+                            <InputText
+                                name="town"
+                                placeholder="Miejscowość"
+                                required={true}
+                                style={{ width: "50%" }}
+                            />
+                        </section>
+                        <InputText
+                            name="address"
+                            placeholder="Ulica"
+                            required={true}
+                        />
+                        <Button>
+                            Złóż zamówienie
+                        </Button>
+                    </form>
                 </Card>
             </Main>
         </Layout>
