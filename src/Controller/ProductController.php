d9f7f8a70e4d4b7cef790b7da404657bb7b8134c
krystiano13 <crystianotv@gmail.com>
2025-01-19 17:50:15 +0100

    refactor create action for Product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class ProductController extends AbstractController
@@ -23,18 +24,13 @@ final class ProductController extends AbstractController
     }
 
     #[Route('/products', name: 'app_products_create', methods: ['POST'])]
-    public function create(Request $request, ValidatorInterface $validator): Response
+    public function create(Request $request, ValidatorInterface $validator, SerializerInterface $sr): Response
     {
         $body = $request -> getContent();
+        $product = $sr -> deserialize($body, Product::class, 'json');
         $messages = array();
         $errors = $validator->validate($body);
         $this->handleErrors($request,$errors, $messages);
-
-        $product = new Product();
-        $product -> setName($body['name']);
-        $product -> setPrice($body['price']);
-        $product -> setAmount($body['amount']);
-        $product -> setImageSrc($body['imageSrc']);
 
         $this -> em -> persist($product);
         $this -> em -> flush();
