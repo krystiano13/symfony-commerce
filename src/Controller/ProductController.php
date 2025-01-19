2546a536e0fce8f566e64ccb3f0029d291fd21ae
krystiano13 <crystianotv@gmail.com>
2025-01-19 18:12:06 +0100

    refactor ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,19 +18,25 @@ final class ProductController extends AbstractController
     use ValidationErrorTrait;
 
     private EntityManagerInterface $em;
+    private ValidatorInterface $validator;
+    private SerializerInterface $serializer;
+    private ProductRepository $pr;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, ValidatorInterface $validator, SerializerInterface $serializer)
     {
         $this -> em = $em;
+        $this -> validator = $validator;
+        $this -> serializer = $serializer;
+        $this -> pr = $this -> em -> getRepository(Product::class);
     }
 
     #[Route('/products', name: 'app_products_create', methods: ['POST'])]
-    public function create(Request $request, ValidatorInterface $validator, SerializerInterface $sr): Response
+    public function create(Request $request): Response
     {
         $body = $request -> getContent();
-        $product = $sr -> deserialize($body, Product::class, 'json');
+        $product = $this -> serializer -> deserialize($body, Product::class, 'json');
         $messages = array();
-        $errors = $validator->validate($body);
+        $errors = $this -> validator->validate($body);
         $this->handleErrors($request,$errors, $messages);
 
         $this -> em -> persist($product);
@@ -40,16 +46,16 @@ final class ProductController extends AbstractController
     }
 
     #[Route('/products/{id}', name: 'app_products_update', methods: ['PATCH'])]
-    public function update(int $id ,Request $request, ValidatorInterface $validator, ProductRepository $pr, SerializerInterface $sr)
+    public function update(int $id ,Request $request)
     {
-        $product = $pr -> find($id);
+        $product = $this -> pr -> find($id);
         $messages = array();
 
         if($product)
         {
             $body = $request -> getContent();
-            $updatedProduct = $sr -> deserialize($body, Product::class, 'json');
-            $errors = $validator->validate($body);
+            $updatedProduct = $this -> serializer -> deserialize($body, Product::class, 'json');
+            $errors = $this -> validator->validate($body);
             $this -> handleErrors($request,$errors, $messages);
 
             $product -> setName($updatedProduct -> getName());
@@ -64,9 +70,9 @@ final class ProductController extends AbstractController
     }
 
     #[Route('/products/{id}', name: 'app_products_delete', methods: ['DELETE'])]
-    public function destroy(int $id, ProductRepository $pr)
+    public function destroy(int $id)
     {
-        $product = $pr -> find($id);
+        $product = $this -> pr -> find($id);
 
         if($product)
         {
